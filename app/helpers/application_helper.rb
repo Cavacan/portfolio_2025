32add6d3a1dc75267877f34e86e912d9d2b49eee
Cavacan <tgenk0901@gmail.com>
2025-02-18 22:49:00 +0900

    Update production env settings01 CSS&config/credetials/

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
-  def asset_exists?(asset)
-    Rails.application.assets.find_asset(asset).present?
+  def asset_exists?(asset_path)
+    if Rails.application.config.assets.compile
+      Rails.application.assets&.find_asset(asset_path).present?
+    else
+      Rails.application.assets_manifest.files.key?(asset_path)
+    end
   end
 end
