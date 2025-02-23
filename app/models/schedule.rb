5d55b9786db94aa8329c2f5e95d3e846c759517b
田中元気 <cavacan@Cavacan-MBP.local>
2025-02-23 19:25:29 +0900

    modifed create action for nil_cases

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,6 @@
 class Schedule < ApplicationRecord
   belongs_to :creator, polymorphic: true
+  validates :title, presence: true
+  validates :notification_period, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :next_notification, presence: true
 end
