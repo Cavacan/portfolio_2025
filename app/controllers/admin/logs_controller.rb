4e754663fe804825d9b6f9d56c2c3fbe2883fac4
田中元気 <cavacan@Cavacan-MBP.local>
2025-03-30 13:26:35 +0900

    add CSV export for logs, set timezone to JST

--- app/controllers/admin/logs_controller.rb
+++ app/controllers/admin/logs_controller.rb
@@ -1,6 +1,37 @@
 class Admin::LogsController < Admin::BaseController
+  require 'csv'
+
   def index
     @q = NotificationLog.ransack(params[:q])
-    @logs = @q.result.includes(:schedule).order(created_at: :desc).page(params[:page])
+    @logs = @q.result.includes(:schedule).order(created_at: :desc)
+
+    respond_to do |format|
+      format.html do
+        @logs = @logs.page(params[:page])
+      end
+      format.csv do
+        send_data generate_csv(@logs),
+          filename: "nortification_logs_#{Time.current.strftime('%Y%m%d_%H%M%S')}.csv"
+      end
+    end
+  end
+
+  private
+
+  def generate_csv(logs)
+    CSV.generate(headers: true) do |csv|
+      csv << ["ID", "タイトル", "周期", "送信日時", "作成日時", "更新日時"]
+
+      logs.each do |log|
+        csv <<[
+          log.id,
+          log.schedule_id,
+          log.schedule.title,
+          log.send_time.strftime('%yY/%m/%d %Y:%M'),
+          log.created_at.strftime('%yY/%m/%d %Y:%M'),
+          log.updated_at.strftime('%yY/%m/%d %Y:%M')
+        ]
+      end
+    end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Portfolio2025
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo'
   end
 end
