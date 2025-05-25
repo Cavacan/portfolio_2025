9b26878faf0160422e940d182e65bfa117c63cce
田中元気 <cavacan@Cavacan-MBP.local>
2025-05-26 04:08:02 +0900

    add preset_schedules

---
+++ app/javascript/presets/schedule_presets.js
@@ -0,0 +1,56 @@
+export const schedulePresets = {
+  item1: {
+    name: '購入 | 7日毎 ',
+    title: '購入：',
+    period: 7,
+    price: 0
+  },
+  item2: {
+    name: '購入 | 14日毎 ',
+    title: '購入：',
+    period: 14,
+    price: 0
+  },
+  item01: {
+    name: '--------',
+    title: '無題',
+    period: 1,
+    price: 0
+  },
+  item3: {
+    name: '作業 | 10日毎 ',
+    title: '作業：',
+    period: 10,
+    price: 0
+  },
+  item4: {
+    name: '作業 | 15日毎 ',
+    title: '作業：',
+    period: 15,
+    price: 0
+  },
+  item5: {
+    name: '作業 | 60日毎 ',
+    title: '作業：',
+    period: 60,
+    price: 0
+  },
+  item6: {
+    name: '作業 | 180日毎 ',
+    title: '作業：',
+    period: 180,
+    price: 0
+  },
+  item02: {
+    name: '--------',
+    title: '無題',
+    period: 1,
+    price: 0
+  },
+  item7: {
+    name: '振込 | 30日毎 ',
+    title: '振込；',
+    period: 30,
+    price: 0
+  }
+}
