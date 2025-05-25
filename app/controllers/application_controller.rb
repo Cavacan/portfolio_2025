6eedf47b261770307ef593f48fc9cc34b391d816
田中元気 <cavacan@Cavacan-MBP.local>
2025-05-25 16:52:51 +0900

    add gem meta_tag, apply sessions view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  include MetaTags::ControllerHelper
   private
 
   def not_authenticated
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,23 @@
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    set_meta_tags(
+      title: 'ログイン | 予定通知アプリ',
+      description: 'スケジュールを管理するにはログインして下さい。',
+      og: {
+        title: 'ログイン | 予定通知アプリ',
+        description: 'スケジュールを管理するにはログインして下さい。',
+        type: 'website',
+        url: request.original_url,
+        image: view_context.image_url('ogp/ogp_default.png')
+      },
+      twitter: {
+        card: 'summary_large_image',
+        title: 'ログイン | 予定通知アプリ',
+        description: 'スケジュールを管理するにはログインして下さい。',
+        image: view_context.image_url('ogp/ogp_default.png')
+      }
+    )
+  end
 
   def create
     user = User.find_by(email: params[:email])
