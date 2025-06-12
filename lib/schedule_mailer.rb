a7cbac996e47a1780ef658f5b9b56ba678e000ff
田中元気 <cavacan@Cavacan-MBP.local>
2025-06-13 07:53:56 +0900

    refactor: reduce AbcSize in log_and_update_schedule

--- lib/schedule_mailer.rb
+++ lib/schedule_mailer.rb
@@ -28,48 +28,58 @@ module ScheduleMailer
                 .distinct
 
     users.find_each do |user|
-      start_date = today + 1.day
-      end_date = today + 7.days
-
-      schedules = user.schedules
-                      .where(next_notification: start_date.beginning_of_day..end_date.end_of_day)
-                      .order(:next_notification)
+      schedules = search_schedules(user, today)
 
       next if schedules.empty?
 
-      hour = user.user_setting.notification_hour || 0
-      min = user.user_setting.notification_minute || 0
-      send_time = Time.zone.now.change(hour: hour, min: min, sec: 0)
-      send_time += 1.hour if send_time < Time.zone.now
-
-      UserMailer.send_schedule_pre_notifications(user, schedules).deliver_later(wait_until: send_time)
+      deliver_schedule_pre_notifications(user, schedules)
     end
   end
 
   def log_and_update_schedule(schedule, user)
+    is_snooze = user.user_settings.need_check_done
+
+    create_notification_log(schedule, is_snooze)
+
     if user.user_settings.need_check_done
-      NotificationLog.create!(
-        schedule_id: schedule.id,
-        send_time: schedule.next_notification,
-        price: schedule.price,
-        is_snooze: true
-      )
       schedule.update!(
         next_notification: Date.tomorrow,
         after_next_notification: Date.tomorrow + 1.day
       )
     else
-      NotificationLog.create!(
-        schedule_id: schedule.id,
-        send_time: schedule.next_notification,
-        price: schedule.price,
-        is_snooze: false
-      )
-
       schedule.update!(
         next_notification: schedule.next_notification + schedule.notification_period.days,
         after_next_notification: schedule.after_next_notification + schedule.notification_period.days
       )
     end
+  end
+
+  private
+
+  def search_schedules(user, today)
+    start_date = today + 1.day
+    end_date = today + 7.days
+
+    user.schedules
+        .where(next_notification: start_date.beginning_of_day..end_date.end_of_day)
+        .order(:next_notification)
+  end
+
+  def deliver_schedule_pre_notifications(user, schedules)
+    hour = user.user_setting.notification_hour || 0
+    min = user.user_setting.notification_minute || 0
+    send_time = Time.zone.now.change(hour: hour, min: min, sec: 0)
+    send_time += 1.hour if send_time < Time.zone.now
+
+    UserMailer.send_schedule_pre_notifications(user, schedules).deliver_later(wait_until: send_time)
+  end
+
+  def create_notification_log(schedule, is_snooze)
+    NotificationLog.create!(
+      schedule_id: schedule.id,
+      send_time: schedule.next_notification,
+      price: schedule.price,
+      is_snooze: is_snooze
+    )
   end
 end
